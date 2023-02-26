0444ec956388097499580e4440f4d406430b0d89
Abdullah Al Noman <codernoman@gmail.com>
2023-02-26 22:52:42 +0600

    first commit

--- Module-5/classObjectMethod.php
+++ Module-5/classObjectMethod.php
@@ -6,12 +6,20 @@ class Human{ //class
     }
 
     function getName(){
-
+        echo "My name is {$this->name}";
     }
 }
 
 $h1 = new Human(); //object
-$h1 -> name = "Abdullah Al Noman"; //Set
+$h2 = new Human();
+
+$h1 -> name = "Abdullah Al Noman \n"; //Set
+$h2->name= "Mohsin Habib";
+
 $h1 -> sayName(); 
+$h2 -> sayName(); 
+
 echo PHP_EOL;
-echo $h1->name; //Get
+// echo $h1->name; //Get
+echo $h1->getName();
+echo $h2->getName();
