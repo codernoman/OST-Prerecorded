813726eac55f0749aea8c10bc272fa86dd4c4c58
codernoman <codernoman@gmail.com>
2023-02-27 16:34:18 +0600

    Third commit

--- Module-4/arrayManipulation.php
+++ Module-4/arrayManipulation.php
@@ -1,17 +1,40 @@
 <?php
 $students = array(
-    "Azad",
-    "Rakib",
-    "Nirjhor"
+    "1",
+    "2",
+    "3"
 );
 
 // $students[2]="Noman";
 // print_r($students);
 
-array_shift(); //for data remove
-array_unshift(); //for data entry
-array_pop(); //for data remove
-array_push(); //for data entry
+// array_shift(); //for data remove
+// array_unshift(); //for data entry
+// array_pop(); //for data remove
+// array_push(); //for data entry
+//(arrayName)[index]="New value";
 
 
-$student = array_pop($students);
+// $student = array_pop($students);
+// echo $student;
+// print_r($students);
+
+// $student = array_shift($students);
+// echo $student.PHP_EOL;
+// print_r($students);
+
+$students[1] = "4";
+print_r($students);
+
+// $student = array_shift($students);
+// echo $student.PHP_EOL;
+// print_r($students);
+
+// array_push($students, "4");
+// print_r($students);
+
+// echo PHP_EOL;
+
+// array_unshift($students, "5");
+// print_r($students);
+
---
+++ Module-4/associativeArray.php
@@ -0,0 +1,15 @@
+<?php
+// $students = [
+//     '1' => "Noman",
+//     '2'=> 'Azad',
+//     '3'=>'Rakib'
+// ];
+// echo $students[1];
+// print_r($students);
+$food = [
+    'vagitable' => 'carrot, capsicam, brocolly',
+    'fruits' => 'orange, banana, apple',
+    'drinks' => 'water, cocacola, juice'
+];
+
+($food);
